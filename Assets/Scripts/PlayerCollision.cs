a8c89ea22f8e01884e4d0aeae29d5ff5347ae728
k1sihyeon <tlgus530@naver.com>
2023-11-13 02:09:14 +0900

    Energy System ..

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,8 +4,14 @@ using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerCollision : NetworkBehaviour {
-    
-    private int playerEnergy = 0;
+
+    private float maxEnergy = 100f;
+    private float energyIncreaseRate = 10f;
+    private float energyResetThreshold = 50f;
+    private float currentEnergy = 0f;
+
+    private float collisionIgnoreTime = 5f;
+    private bool ignoringCollisions = false;
 
     private void Ultimate() {
         Debug.Log("Ultimate");
@@ -14,49 +20,58 @@ public class PlayerCollision : NetworkBehaviour {
     }
 
     private void Start() {
-        
+        if (IsServer) {
+            currentEnergy = 0f;
+            ignoringCollisions = false;
+        }
+        else {
+            // 에너지 및 충돌 무시 상태를 클라이언트로부터 동기화
+            UpdateEnergyClientRpc(currentEnergy);
+            UpdateCollisionIgnoreStatusClientRpc(ignoringCollisions);
+        }
     }
 
-    private void Update() {
-        if (!IsClient) return;
 
-        if(IsLocalPlayer) {
-            if (playerEnergy >= 100) {
-                playerEnergy = 0;
-
-                Ultimate();
-            }
-        }
-
-        
-
+    private void NetworkDestroy(Collision collision) {
+        ulong enemyNetworkId = collision.gameObject.GetComponent<NetworkObject>().NetworkObjectId;
+        Destroy(collision.gameObject);
+        CollisionClientRpc(enemyNetworkId);
     }
 
     private void OnCollisionEnter(Collision collision) {
         if (!IsServer) return;
 
-        if (collision.gameObject.CompareTag("Enemy")) {
-            Debug.Log("Collision Enterd");
+        if (!ignoringCollisions) {
 
-            playerEnergy += 50;
-            Debug.Log("Player Energy: " + playerEnergy);
+            if (collision.gameObject.CompareTag("Enemy")) {
+                energyIncreaseRate = 50f;
+                NetworkDestroy(collision);
+            }
 
-            ulong enemyNetworkId = collision.gameObject.GetComponent<NetworkObject>().NetworkObjectId;
-            Destroy(collision.gameObject);
-            CollisionClientRpc(enemyNetworkId);
+            if (collision.gameObject.CompareTag("BallObstacle")) {
+                energyIncreaseRate = 100f;
+                NetworkDestroy(collision);
+            }
+
+            currentEnergy += energyIncreaseRate;
+
+            //Ultimate
+            if (currentEnergy >= energyResetThreshold) {
+
+                Debug.Log("Ultimate");
+
+                ignoringCollisions = true;
+                StartCoroutine(ResetCollisionIgnore());
+                currentEnergy = 0f;
+            }
+
         }
 
-        if (collision.gameObject.CompareTag("BallObstacle")) {
-            Debug.Log("Collision Enterd");
+    }
 
-            playerEnergy += 100;
-            Debug.Log("Player Energy: " + playerEnergy);
-
-            ulong enemyNetworkId = collision.gameObject.GetComponent<NetworkObject>().NetworkObjectId;
-            Destroy(collision.gameObject);
-            CollisionClientRpc(enemyNetworkId);
-        }
-
+    private IEnumerator ResetCollisionIgnore() {
+        yield return new WaitForSeconds(collisionIgnoreTime);
+        ignoringCollisions = false;
     }
 
 
@@ -66,5 +81,15 @@ public class PlayerCollision : NetworkBehaviour {
             NetworkObject enemyNetworkObject = NetworkManager.Singleton.SpawnManager.SpawnedObjects[enemyNetworkId];
             Destroy(enemyNetworkObject.gameObject);
         //}
+    }
+
+    [ClientRpc]
+    private void UpdateEnergyClientRpc(float energy) {
+        currentEnergy = energy;
+    }
+
+    [ClientRpc]
+    private void UpdateCollisionIgnoreStatusClientRpc(bool ignoreStatus) {
+        ignoringCollisions = ignoreStatus;
     }
 }
