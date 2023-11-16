fb5969230f21ae6424e90b8ecbcd59049a53d386
k1sihyeon <tlgus530@naver.com>
2023-11-16 11:10:11 +0900

    added LobbyScene

---
+++ Assets/Scripts/Lobby/UnityServiceAuthenticator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.Services.Authentication;
+
+namespace Hi5ive {
+
+    public class UnityServiceAuthenticator : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
+
