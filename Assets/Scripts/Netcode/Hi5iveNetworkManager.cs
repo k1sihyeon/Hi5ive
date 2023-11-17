b279f54e9f67f69f1fa650493f47b8551567c38f
k1sihyeon <tlgus530@naver.com>
2023-11-17 17:32:19 +0900

    Physics Layer Setting

--- Assets/Scripts/Netcode/Hi5iveNetworkManager.cs
+++ Assets/Scripts/Netcode/Hi5iveNetworkManager.cs
@@ -18,11 +18,6 @@ public class Hi5iveNetworkManager : MonoBehaviour {
         //이유는 모르겠지만 ServerListenAddress는 개발단계에서만 사용하라고 권고하고 있음
     }
 
-
-    private void HandleServerStarted() {
-        networkManager.SceneManager.LoadScene("Stage1TestingScene", LoadSceneMode.Single);
-    }
-
     private void Awake() {
         DontDestroyOnLoad(this);
 
