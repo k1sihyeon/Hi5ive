e8a0b6da4effdcf42a8f93b865b6f30d828b421d
k1sihyeon <tlgus530@naver.com>
2023-12-01 16:00:17 +0900

    [UPDATE] Login Scene - Animation

--- Assets/Scripts/Login/LoginPlayerController.cs
+++ Assets/Scripts/Login/LoginPlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class LoginPlayerController : MonoBehaviour {
 
-    public Transform[] waypoints;  // 미리 정해진 원 형태의 경로의 위치들
-    public float moveSpeed = 8f;
+    public Transform[] waypoints;  
+    public float moveSpeed = 4f;
     public float jumpForce = 6f;
 
     private int currentWaypointIndex = 0;
@@ -17,37 +17,27 @@ public class LoginPlayerController : MonoBehaviour {
 
     [Header("Animation Smoothing")]
     [Range(0, 1f)]
-    public float HorizontalAnimSmoothTime = 0.2f;
-    [Range(0, 1f)]
-    public float VerticalAnimTime = 0.2f;
-    [Range(0, 1f)]
     public float StartAnimTime = 0.3f;
     [Range(0, 1f)]
     public float StopAnimTime = 0.15f;
 
     void Start() {
         animator = GetComponent<Animator>();
+        currentWaypointIndex = Random.Range(0, waypoints.Length);
     }
 
     void Update() {
-        // 캐릭터를 원 형태의 경로로 이동
         Vector3 targetPosition = waypoints[currentWaypointIndex].position;
-        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        probability = Random.value;
+        if (probability < 0.001f) {
+            Jump();
+        }
 
         // 만약 현재 위치와 목표 위치가 거의 같다면 다음 위치로 이동
-        if (Vector3.Distance(transform.position, targetPosition) < 0.1f) {
-            probability = Random.value;
-            if (probability < 0.2f)  // 홀수번째 위치일 때 점프
-            {
-                Jump();
-            }
-
-            currentWaypointIndex++;
-
-            // 배열의 끝에 도달하면 처음 위치로 이동
-            if (currentWaypointIndex >= waypoints.Length) {
-                currentWaypointIndex = 0;
-            }
+        if (Vector3.Distance(transform.position, targetPosition) < 0.2f) {
+            currentWaypointIndex = Random.Range(0, waypoints.Length);
         }
 
         // 애니메이션 업데이트
@@ -56,15 +46,13 @@ public class LoginPlayerController : MonoBehaviour {
 
     void Jump() {
         if (!isJumping) {
-            // 점프 로직
-            GetComponent<Rigidbody>().AddForce(new Vector2(0f, jumpForce), ForceMode.Impulse);
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isJumping = true;
             animator.SetTrigger("jump");
         }
     }
 
     void OnCollisionEnter(Collision collision) {
-        // 땅에 닿았을 때
         if (collision.gameObject.CompareTag("Ground")) {
             isJumping = false;
         }
@@ -72,7 +60,9 @@ public class LoginPlayerController : MonoBehaviour {
 
     void UpdateAnimator() {
         // 이동 애니메이션 업데이트
-        float horizontalInput = (currentWaypointIndex % 2 == 0) ? 0f : 1f;  // 홀수 위치에서만 이동
-        animator.SetFloat("Blend", horizontalInput, StartAnimTime, Time.deltaTime);
+        animator.SetFloat("Blend", 1f, StartAnimTime, Time.deltaTime);
+
+        Vector3 targetDirection = (waypoints[currentWaypointIndex].position - transform.position).normalized;
+        transform.LookAt(transform.position + new Vector3(targetDirection.x, 0f, targetDirection.z), Vector3.up);
     }
 }
