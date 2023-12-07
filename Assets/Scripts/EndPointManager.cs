5f72f4f884472693be4edb05edb4c9f8db018438
k1sihyeon <tlgus530@naver.com>
2023-12-07 12:05:19 +0900

    [UPDATE] EndPointManager

--- Assets/Scripts/EndPointManager.cs
+++ Assets/Scripts/EndPointManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
@@ -8,6 +9,7 @@ using UnityEngine;
 public class EndPointManager : NetworkBehaviour {
 
     [SerializeField] private int rank = 0;
+    private List<ulong> ranks = new List<ulong>();
     private PlayerEndPoint playerEndPoint;
     private bool coolDown = true;
 
@@ -23,26 +25,41 @@ public class EndPointManager : NetworkBehaviour {
 
 
     private void OnCollisionEnter(Collision collision) {
-        if (!IsServer)
-            return;
+        //if (!IsServer)
+        //    return;
 
-        
+
         if (collision.gameObject.CompareTag("Player")) {
+            NetworkObject networkObject = collision.gameObject.GetComponent<NetworkObject>();
 
-            if (coolDown) {
-                coolDown = false;
-
-                rank += 1;
-
-                Debug.Log("world Rank: " + rank);
-
-                UpdateRankUI(rank);
-
-                ResetCoolDown(1f);
+            if (!ranks.Exists(x => x == networkObject.OwnerClientId)) {
+                ranks.Add(networkObject.OwnerClientId);
             }
 
-            
+            //UpdateRanksServerRpc(ranks);
+
+            rank = ranks.IndexOf(networkObject.OwnerClientId) + 1;
+
+            collision.gameObject.GetComponent<PlayerEndPoint>().rank = rank;
+            UpdateRankUI(rank);
         }
+
+
+        //if (collision.gameObject.CompareTag("Player")) {
+
+        //    if (coolDown) {
+        //        coolDown = false;
+
+        //        rank += 1;
+
+        //        Debug.Log("world Rank: " + rank);
+
+        //        UpdateRankUI(rank);
+
+        //        ResetCoolDown(1f);
+        //    }
+
+        //}
     }
 
     private IEnumerator ResetCoolDown(float delay) {
@@ -66,6 +83,16 @@ public class EndPointManager : NetworkBehaviour {
         }
     }
 
+    //[ServerRpc]
+    //private void UpdateRanksServerRpc(List<ulong> ranks) {
+    //    this.ranks = ranks;
+    //    UpdateRanksClientRpc(ranks);
+    //}
+
+    //[ClientRpc]
+    //private void UpdateRanksClientRpc(List<ulong> ranks) {
+    //    this.ranks = ranks;
+    //}
 
 
 }
